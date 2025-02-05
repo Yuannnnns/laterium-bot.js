2530a5e031da9c77cc3c19a02e0985a5e44d6fe5
Yuannnnns <barokahsukawangi@gmail.com>
2025-02-05 10:04:31 +0700

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -4,11 +4,11 @@ const mysql = require('mysql2');
 const colors = require('colors');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectTimeout: 20000
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_NAME,
+    connectTimeout: 20000 // @default
 });
 
 connection.connect((err) => {
