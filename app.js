006730466fa55f098b03203f1a27499d037de75a
Yuannnnns <barokahsukawangi@gmail.com>
2025-02-05 12:30:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -145,25 +145,8 @@ for (const file of sampQueryFiles) {
 }
 
 /// @system : Character Menu
-app.on('interactionCreate', async (interaction) => {
-    if (!interaction.isStringSelectMenu()) return;
-
-    if (interaction.customId === 'character_menu') {
-        const c_inter = interaction.values[0];
-
-        if (c_inter === 'new_ucp') {
-            await interaction.reply({ content: 'You selected **New UCP**.', ephemeral: true });
-        } else if (c_inter === 'new_ucp_code') {
-            await interaction.reply({ content: 'You selected **New UCP Code**.', ephemeral: true });
-        } else if (c_inter === 'new_password') {
-            await interaction.reply({ content: 'You selected **New Password**.', ephemeral: true });
-        } else if (c_inter === 'delete_character') {
-            await interaction.reply({ content: 'You selected **Delete Character**.', ephemeral: true });
-        }
-
-        console.log(`[LOG INTER] ${interaction.user.tag} selected ${c_inter} in #${interaction.channel.name}`);
-    }
-});
+const interactionHandler = require('./interactionHandler');
+client.on('interactionCreate', interactionHandler);
 
 /// @System : Timeout New Member's
 const TIMEOUT_DURATION = 8 * 60 * 1000; // 8-minutes
