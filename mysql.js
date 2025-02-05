69f1d378c591ebdfbd7057734b81340e7b769986
Yuannnnns <barokahsukawangi@gmail.com>
2025-02-05 13:10:30 +0700

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -1,22 +1,15 @@
 /// @proj : mysql.js
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const colors = require('colors');
 
-const connection = mysql.createConnection({
-    host: process.env.SQL_HOST,
+const connection = mysql.createPool({
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_NAME,
-    connectTimeout: 20000 // @default
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error('\nError connecting to database:'.red, err.stack);
-        return;
-    }
-    console.log('Connected to database with ID:'.green, connection.threadId);
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = connection;
